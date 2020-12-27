62b92e8b632a8557abe6f658ccd7675d471214bc
Dawn <dawn.wages@gmail.com>
2020-12-27 07:41:56 -0500

    homepage work

--- dawnwagesinfo/static/js/dawnwagesinfo.js
+++ dawnwagesinfo/static/js/dawnwagesinfo.js
@@ -1,19 +1,18 @@
-let navState = 'open';
+let navState = 'close';
 
-function openNav() {
 
-}
-
-function closeNav() {
+function closeNav(e) {
     if (navState == 'open') {
         document.getElementById("mySidenav").style.width = "0vw";
-        document.getElementById("opaqueContainer").style.backgroundColor = "transparent";
-        console.log('close');
+        document.getElementById("l1").style.transform = null;
+        document.getElementById("l2").style.transform = null;
+        document.getElementById("l3").style.display = 'inherit';
         navState = 'close';
     } else {
-        document.getElementById("mySidenav").style.width = "inherit";
-        document.getElementById("opaqueContainer").style.backgroundColor = null;
-        console.log('open');
+        document.getElementById("mySidenav").style.width = "100vw";
+        document.getElementById("l1").style.transform = 'matrix(1.1, -1, 0, 1, -1, 10)';
+        document.getElementById("l2").style.transform = 'matrix(1.79, 1, 0, 1, 0, -10)';
+        document.getElementById("l3").style.display = 'none';
         navState = 'open';
     }
 
--- home/models.py
+++ home/models.py
@@ -1,7 +1,21 @@
 from django.db import models
 
 from wagtail.core.models import Page
+from wagtail.core.fields import RichTextField
+from wagtail.admin.edit_handlers import FieldPanel
 
 
 class HomePage(Page):
-    pass
+    name = models.CharField(max_length=255, blank=True)
+    job_title = models.CharField(max_length=255, blank=True)
+    desc = models.CharField(max_length=255, blank=True)
+    loc = models.CharField(max_length=255, blank=True)
+    desc_long = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('name'),
+        FieldPanel('job_title'),
+        FieldPanel('desc'),
+        FieldPanel('loc'),
+        FieldPanel('desc_long', classname="full")
+    ]
