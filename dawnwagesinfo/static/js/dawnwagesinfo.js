2fa31aab2fc18393fc014efa6d21c465992dfae2
Dawn <dawn.wages@gmail.com>
2020-12-29 03:12:00 -0500

    mobile nav and conditional buttons on portfolio

--- dawnwagesinfo/static/js/dawnwagesinfo.js
+++ dawnwagesinfo/static/js/dawnwagesinfo.js
@@ -1,22 +1,33 @@
 let navState = 'close';
-
+let mobile = false;
+let sideNavWidth = 24;
+let smallScreenWidth = 15;
+let xSmallScreenWidth = 10;
 
 function closeNav(e) {
+    if (screen.width < 482) {
+        sideNavWidth = smallScreenWidth;
+    }
+
+    if (screen.width < 322){
+        sideNavWidth = xSmallScreenWidth;
+    }
+
     if (navState == 'open') {
         document.getElementById("mySidenav").style.width = "0vw";
         document.getElementById("l1").style.transform = null;
         document.getElementById("l2").style.transform = null;
         document.getElementById("l3").style.display = 'inherit';
-        document.getElementById("side-nav").style.right = '10rem';
+        document.getElementById("side-nav").style.right = "5rem";
         navState = 'close';
 
     } else {
-        document.getElementById("mySidenav").style.width = "24rem";
+        document.getElementById("mySidenav").style.width = sideNavWidth + "rem";
         document.getElementById("l1").style.transform = 'matrix(1, -1, 0, 1.1, 0, 9)';
         document.getElementById("l2").style.transform = 'matrix(1, 1, 0, 1, .5, -17)';
         document.getElementById("l3").style.display = 'none';
-        document.getElementById("side-nav").style.right = '26rem';
+        document.getElementById("side-nav").style.right = sideNavWidth + 2 + "rem";
         navState = 'open';
     }
- console.log(navState);
+ console.log(navState, screen.width, sideNavWidth);
 }
