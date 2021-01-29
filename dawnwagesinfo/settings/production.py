1c726f6685d4a377c2e108891f35571dedf7ee15
Dawn <dawn.wages@gmail.com>
2021-01-29 00:08:51 -0500

    prod db

--- dawnwagesinfo/settings/production.py
+++ dawnwagesinfo/settings/production.py
@@ -10,9 +10,9 @@ except ImportError:
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',  # 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'dawnwagesinfo',                      # Or path to database file if using sqlite3.
-        'USER': 'user',                      # Not used with sqlite3.
-        'PASSWORD': 'ohSh111t',                  # Not used with sqlite3.
+        'NAME': 'django',                      # Or path to database file if using sqlite3.
+        'USER': 'django',                      # Not used with sqlite3.
+        'PASSWORD': '2706e1f5e33a8a797895993d8b82fa4c',                  # Not used with sqlite3.
         'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     },
