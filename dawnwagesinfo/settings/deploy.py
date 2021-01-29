0cb3b267122fb511871a88b875f9e2199c749059
Dawn <dawn.wages@gmail.com>
2021-01-29 02:28:50 -0500

    db setting changed for prod

--- dawnwagesinfo/settings/deploy.py
+++ dawnwagesinfo/settings/deploy.py
@@ -33,9 +33,9 @@ except ImportError:
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',  # 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'django',                      # Or path to database file if using sqlite3.
-        'USER': 'django',                      # Not used with sqlite3.
-        'PASSWORD': '2706e1f5e33a8a797895993d8b82fa4c',                  # Not used with sqlite3.
+        'NAME': 'dawnwagesinfo',                      # Or path to database file if using sqlite3.
+        'USER': 'dawnwagesinfo_user',                      # Not used with sqlite3.
+        'PASSWORD': 'ohSh111t',                  # Not used with sqlite3.
         'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     },
