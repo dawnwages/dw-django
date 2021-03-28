e6575f628954807b4e0fbbaecebe3cf06f01666b
Dawn <dawn.wages@gmail.com>
2021-03-28 12:18:17 -0400

    deploy settings CL

--- dawnwagesinfo/settings/deploy.py
+++ dawnwagesinfo/settings/deploy.py
@@ -1,6 +1,7 @@
 from socket import gethostname, gethostbyname
 from .base import *
 import dj_database_url
+import django_heroku
 
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
@@ -10,3 +11,6 @@ TEMPLATE_DEBUG = DEBUG
 
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
