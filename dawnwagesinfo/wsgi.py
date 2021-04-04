baad95850ae17ccbe073d01fbbb230e180fee0db
Dawn <dawn.wages@gmail.com>
2021-04-04 17:14:24 -0400

    whitenoise root dir

--- dawnwagesinfo/wsgi.py
+++ dawnwagesinfo/wsgi.py
@@ -15,4 +15,4 @@ from whitenoise import WhiteNoise
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dawnwagesinfo.settings.dev")
 
 application = get_wsgi_application()
-application = WhiteNoise(application, root='/static/')
+application = WhiteNoise(application, root='static')
