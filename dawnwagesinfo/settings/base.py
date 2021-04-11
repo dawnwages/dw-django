c2f5aae2cb35b175ba471ac0b7b6afb065b9ee55
Dawn <dawn.wages@gmail.com>
2021-04-10 21:30:54 -0400

    fix staticfiles_dir

--- dawnwagesinfo/settings/base.py
+++ dawnwagesinfo/settings/base.py
@@ -156,7 +156,7 @@ STATICFILES_FINDERS = [
 ]
 
 STATICFILES_DIRS = [
-    os.path.join(PROJECT_DIR, 'static'),
+    os.path.join(PROJECT_DIR, 'staticfiles'),
     os.path.join(BASE_DIR, 'frontend/build'), # TODO: Figure out if you need this second path
 ]
 
