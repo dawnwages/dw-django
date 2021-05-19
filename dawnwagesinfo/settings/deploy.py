7a4dce687cbb41ea4a355307a63474ba0ab14a57
Dawn <dawn.wages@gmail.com>
2021-05-18 22:11:58 -0400

    deploy allowed hosts

--- dawnwagesinfo/settings/deploy.py
+++ dawnwagesinfo/settings/deploy.py
@@ -9,8 +9,6 @@ from logdna import LogDNAHandler
 DEBUG = os.environ.get('DEBUG')
 TEMPLATE_DEBUG = DEBUG
 
-ALLOWED_HOSTS = ["dawnwages.info", "www.dawnwages.info"]
-
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
 
@@ -42,15 +40,6 @@ else:
 
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-
-ALLOWED_HOSTS = [
-    'localhost',
-    '127.0.0.1',
-    gethostname(),
-    gethostbyname(gethostname()),
-    'logs.dna.com',
-    'dawn-wages-info.herokuapp.com'
-]
 
 DEBUG_PROPAGATE_EXCEPTIONS = True
 COMPRESS_ENABLED = os.environ.get('COMPRESS_ENABLED', False)
