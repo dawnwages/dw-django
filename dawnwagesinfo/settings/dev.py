1b8e62d1a05ae590e28e824c935d71a785b8a88a
Dawn <dawn.wages@gmail.com>
2022-07-04 18:08:55 -0400

    footer icon size

--- dawnwagesinfo/settings/dev.py
+++ dawnwagesinfo/settings/dev.py
@@ -3,7 +3,7 @@ from .base import *
 
 ENVIRONMENT = os.environ.get("ENVIRONMENT", "local")
 
-DEBUG = False
+DEBUG = True
 # SECURITY WARNING: don't run with debug turned on in production!
 TEMPLATE_DEBUG = DEBUG
 
