396e095b1a51eb756af675cc1b7c8655c60956df
dawnwages <dawn.wages@gmail.com>
2026-05-22 00:01:43 -0400

    settings

--- dawnwagesinfo/settings/local.py
+++ dawnwagesinfo/settings/local.py
@@ -10,14 +10,14 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 CALENDAR_DOMAIN = '0.0.0.0:8000'
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',  # 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'dawnwagesinfo',                      # Or path to database file if using sqlite3.
-        'USER': 'postgres',                      # Not used with sqlite3.
-        'PASSWORD': '4',                  # Not used with sqlite3.
-        'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    },
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',  # 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+    #     'NAME': 'dawnwagesinfo',                      # Or path to database file if using sqlite3.
+    #     'USER': 'postgres',                      # Not used with sqlite3.
+    #     'PASSWORD': '4',                  # Not used with sqlite3.
+    #     'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
+    #     'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    # },
 }
 
 UPLOAD_ROOT = os.path.abspath(os.path.join(BASE_DIR, "uploads"))
