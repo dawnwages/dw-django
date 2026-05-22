2743149019079cf1699a758ead07e351669232bb
dawnwages <dawn.wages@gmail.com>
2026-05-22 00:03:39 -0400

    settings

--- dawnwagesinfo/wsgi.py
+++ dawnwagesinfo/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dawnwagesinfo.settings.dev")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dawnwagesinfo.settings.deploy")
 
 application = get_wsgi_application()
